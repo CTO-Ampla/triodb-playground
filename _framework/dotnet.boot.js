dfc04868461544e74eadde6738a3e5ca280a8403
CTO-Ampla <151040595+CTO-Ampla@users.noreply.github.com>
2026-02-24 13:56:19 +0000

    Fix

--- _framework/dotnet.boot.js
+++ _framework/dotnet.boot.js
@@ -1,7 +1,7 @@
 export const config = /*json-start*/{
   "mainAssemblyName": "TriDB.Wasm.dll",
   "resources": {
-    "hash": "sha256-cSZRWdYCcZp1GSZmvavRpDzsN0BeRStq+xqlMqS7jbU=",
+    "hash": "sha256-yRuJGMv+LVJjnP9G/u80jCD1rarDqE4fQhb1DUAaXho=",
     "jsModuleNative": [
       {
         "name": "dotnet.native.js"
@@ -15,7 +15,7 @@ export const config = /*json-start*/{
     "wasmNative": [
       {
         "name": "dotnet.native.wasm",
-        "integrity": "sha256-fYxwbrLZuQSoyAuuzkcJJGxWY0oXqtb7FaqfN0H88M4="
+        "integrity": "sha256-hgoKw/p+nDzhO+WbIrXD8WFuxbHS6eNrc+gv8H4ydOE="
       }
     ],
     "coreAssembly": [
@@ -32,12 +32,12 @@ export const config = /*json-start*/{
       {
         "virtualPath": "System.Linq.wasm",
         "name": "System.Linq.wasm",
-        "integrity": "sha256-WrtueNOgpXkAMsbgZ0CYpGE6sLq064wXmAFeXfj/xao="
+        "integrity": "sha256-ld1HWSv8iDAgtKvfhkSsGhbQCKXSnPVZGKhNzUC90aU="
       },
       {
         "virtualPath": "System.Private.CoreLib.wasm",
         "name": "System.Private.CoreLib.wasm",
-        "integrity": "sha256-2a7E5oUtEge6kjtWEYwoscJFhym+Pk90LCjyvczvIwA="
+        "integrity": "sha256-ANX7uQDJl8ktp4RInLwB571v3MIS5PE0CarVwrOX6dU="
       },
       {
         "virtualPath": "System.Runtime.InteropServices.JavaScript.wasm",
@@ -47,52 +47,52 @@ export const config = /*json-start*/{
       {
         "virtualPath": "TriDB.Core.wasm",
         "name": "TriDB.Core.wasm",
-        "integrity": "sha256-W4DbnhZhCc8u3uG2oC1dekCknoHGgXoEvMWzU/QQulE="
+        "integrity": "sha256-GWy3/qW2YfXJZ6tiCSZ7AFtuwho1QLq1kudUDhvRK5w="
       },
       {
         "virtualPath": "TriDB.Distance.wasm",
         "name": "TriDB.Distance.wasm",
-        "integrity": "sha256-s1iV1yrYPp8UpzSu9uxBvw4HTwP3n2cRsXkeB3BzKU0="
+        "integrity": "sha256-1uL0XvCl8Iq8TGYR/vNWSpN1Te/0gNbFvThmPmBp6cI="
       },
       {
         "virtualPath": "TriDB.Engine.wasm",
         "name": "TriDB.Engine.wasm",
-        "integrity": "sha256-hWkeujxsh72kEpU1TUiR2KaDxFWxcjDLHn1Yj1x+9yc="
+        "integrity": "sha256-kzK0JGgKeRIhnX2WOpfC1TnKDVCvqLR/7d0xLHtiOCQ="
       },
       {
         "virtualPath": "TriDB.Indexes.wasm",
         "name": "TriDB.Indexes.wasm",
-        "integrity": "sha256-pcaafpXohodQA/lNOzmpdvS5QcQPQnTkfHnrnlpimek="
+        "integrity": "sha256-1xs8vjvZ5+6To8gvSlYtxaHXdQjOnU3b5KCDQSN9+rk="
       },
       {
         "virtualPath": "TriDB.Language.wasm",
         "name": "TriDB.Language.wasm",
-        "integrity": "sha256-ILqBoYjU6P+rbm5Mex/PxOzWs2pGq0ShrFoa6bc2FHs="
+        "integrity": "sha256-GyftShoqe3UcilauJGwuR3nlpumpJm49/GVgkRddYDY="
       },
       {
         "virtualPath": "TriDB.LanguageService.wasm",
         "name": "TriDB.LanguageService.wasm",
-        "integrity": "sha256-sYgMEh7H3uUOOskEhLwl3P85mgrTSJaHpu6DmOK4Brw="
+        "integrity": "sha256-PLOVzVNq54Zw1cO+JvP/7YieUwNDeNMfytvOUJptVQ4="
       },
       {
         "virtualPath": "TriDB.Query.wasm",
         "name": "TriDB.Query.wasm",
-        "integrity": "sha256-+JGhGdzYiynjD3NzCrBFK9KQ/Ae3IK8EA9rpLNpCxYI="
+        "integrity": "sha256-TAibCSsBLXeykEg18ohTQ4phj5LyFFlCoX8DtydbRdU="
       },
       {
         "virtualPath": "TriDB.Schema.wasm",
         "name": "TriDB.Schema.wasm",
-        "integrity": "sha256-O9RIXL87xYOijlyzA0uvFZmbG+3jiiAMh8Iemx/3dR0="
+        "integrity": "sha256-/MyieEBGuGJOlhhLXTOCQp5o1wdE3iGfQx8nYyy/MeY="
       },
       {
         "virtualPath": "TriDB.Storage.wasm",
         "name": "TriDB.Storage.wasm",
-        "integrity": "sha256-BLbK/jKRYQ47X0N47HmEGablUxwr2hqlDEYx3tPGjug="
+        "integrity": "sha256-gfRW4FEylAZBE1JcpS9vNKnKGJGsfJiJtL4YiJSDfZE="
       },
       {
         "virtualPath": "TriDB.Wasm.wasm",
         "name": "TriDB.Wasm.wasm",
-        "integrity": "sha256-3I7UgQrH2fTqCX10a/7tTWYKuQM3PfM8AF32Xb2MF6Q="
+        "integrity": "sha256-80qJIY4GMYmmwUA21uVWImtkJw9qwpXwcJoTVW67nKU="
       },
       {
         "virtualPath": "aot-instances.wasm",
